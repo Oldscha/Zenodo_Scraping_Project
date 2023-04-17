6dda6297e81aaa72db94e0814b6b37ff78aa1375
Oldscha <olga.shpakova@hotmail.com>
2023-04-17 12:07:03 +0200

    Remove authors and add journals to dataframe

--- scripts/02-paper_scraping.r
+++ scripts/02-paper_scraping.r
@@ -9,7 +9,7 @@ status <- "published"
 size <- "1000"
 
 # Create empty dataframe with the columns we want
-publications_df <- data.frame(title = character(), authors = character(), date = character(), description = character(), keywords = character(), community = character())
+publications_df <- data.frame(title = character(), journal = character(), date = character(), description = character(), keywords = character(), community = character())
 
 for(community_id in filtered_communities$id) {
   endpoint_url <- glue("https://zenodo.org/api/records/?status={status}&size={size}&communities={community_id}")
@@ -17,20 +17,14 @@ for(community_id in filtered_communities$id) {
   records <- content(request, as = "parsed")
     
   for (record in records) {
-    title <- record$metadata$title
-    authors <- record$metadata$creators
-    
-    author_names <- c()
-    for (author in authors) {
-      author_names <- c(author_names, c(author$name))
-    }
-    
-    author_names <- paste(author_names, collapse = ', ')
-    date <- record$metadata$publication_date
+    titles <- record$metadata$title
+    journal_titles <- record$metadata$journal_title
+
+    publication_dates <- record$metadata$publication_date
     keywords <- paste(record$metadata$keywords, collapse = ', ')
     
     publications_df <- publications_df %>% 
-      add_row(title = title, authors = author_names, date = date, keywords = keywords, community = rep(community_id, length(records)))
+      add_row(title = title, journal = journal_titles, date = publication_dates, keywords = keywords, community = rep(community_id, length(records)))
   }
   
 }
